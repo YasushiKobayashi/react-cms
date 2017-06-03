507c8441e952a989972a75359fd2a6034e43b014
Kobayashi(attela) <kobayashi@attela.jp>
2017-06-03 22:33:33 +0900

    fix sort & sass

--- src/ssrComponents/Loading/index.jsx
+++ src/ssrComponents/Loading/index.jsx
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 
-// import style
 export default class Loading extends Component {
   render() {
     return (
-      // <div className={style.}>
       <div>
         loading now
       </div>
