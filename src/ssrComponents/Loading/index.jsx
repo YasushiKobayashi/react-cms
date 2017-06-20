0c685381fa7c4064251f5c15d82340828b2f1d78
Kobayashi(attela) <kobayashi@attela.jp>
2017-06-20 13:52:46 +0900

    ssrの値が引き継がれるバグの修正

--- src/ssrComponents/Loading/index.jsx
+++ src/ssrComponents/Loading/index.jsx
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
-import MDSpinner from 'react-md-spinner';
 
 export default class Loading extends Component {
   render() {
     return (
-      <div>
-        <MDSpinner />
-      </div>
+      <div />
     );
   }
 }
