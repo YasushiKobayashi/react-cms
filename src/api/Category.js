d08060df6f0f2e3b9c68e3fd33821c3c69c8efd6
YasushiKobayashi <ptpadan@gmail.com>
2017-08-29 15:57:18 +0900

    url修正 ssrの停止

--- src/api/Category.js
+++ src/api/Category.js
@@ -4,7 +4,7 @@ import { Category } from '../model';
 export default class {
   static get(token = null) {
     return new Promise((resolve, reject) => {
-      request.GET(apiUrl('v1', 'category'), token).then((arr) => {
+      request.GET(apiUrl('v1', 'categories'), token).then((arr) => {
         resolve(arr.map((obj) => {
           return new Category(obj);
         }));
@@ -17,7 +17,7 @@ export default class {
 
   static post(params) {
     return new Promise((resolve, reject) => {
-      request.POST(apiUrl('v1', 'category'), params).then((obj) => {
+      request.POST(apiUrl('v1', 'categories'), params).then((obj) => {
         resolve(new Category(obj));
       }).catch((err) => {
         reject(err);
--- src/serverContent.jsx
+++ src/serverContent.jsx
@@ -15,24 +15,24 @@ export default (async (props, url) => {
     const routes = props.router.routes;
     const routerParams = props.router.routerParams;
     const displayName = routes[1].component.displayName;
-    const param = params.getParam(url);
-    const query = param ? `?${param}` : '';
-    const decodeQuery = params.decode(param);
+    // const param = params.getParam(url);
+    // const query = param ? `?${param}` : '';
+    // const decodeQuery = params.decode(param);
 
     // ユーザー関連
-    state.app.user = await User.get('user');
-    state.app.isLoading = false;
-    state.app.isLogin = true;
-    state.app.isSsr = true;
+    // state.app.user = await User.get('user');
+    // state.app.isLoading = false;
+    // state.app.isLogin = true;
+    // state.app.isSsr = true;
 
     // top
     if (displayName.match(/Top/)) {
-      state.top.archives = await Archive.getAllArticle(`post${query}`);
-      state.top.count = await Archive.count();
-      state.top.categories = await Category.get();
-      state.top.pageNumber = decodeQuery.pages;
-      state.top.isLoading = false;
-      state.top.isSsr = true;
+      // state.top.archives = await Archive.getAllArticle(`post${query}`);
+      // state.top.count = await Archive.count();
+      // state.top.categories = await Category.get();
+      // state.top.pageNumber = decodeQuery.pages;
+      // state.top.isLoading = false;
+      // state.top.isSsr = true;
       title = constant.TITLE_TOP;
     }
 
@@ -40,9 +40,9 @@ export default (async (props, url) => {
     if (displayName.match(/Article/)) {
       const id = routerParams.id;
       const article = await Archive.getSigleArticle(id);
-      state.article.article = article;
-      state.article.isLoading = false;
-      state.article.isSsr = true;
+      // state.article.article = article;
+      // state.article.isLoading = false;
+      // state.article.isSsr = true;
       title = article.title;
     }
 
